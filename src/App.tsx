9b5b7ce02ce380d18bc411630781a0eb0d6b0445
Alexander Danilov <hrbrd78@gmail.com>
2022-12-31 17:16:22 +0300

    add basic structure

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+const StyledTetrisWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
+  outline: none;
+`;
+
+const StyledTetris = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px;
+  margin: 0 auto;
+  .display {
+    display: flex;
+    justify-content: space-between;
+    width: 380px;
+  }
+`;
+
+function App() {
+  return (
+    <StyledTetrisWrapper role="button" tabIndex={0}>
+      <StyledTetris>Start here!</StyledTetris>
+    </StyledTetrisWrapper>
+  );
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createGlobalStyle } from 'styled-components';
+
+import App from './App';
+import bg from './assets/bg.jpg';
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background: url(${bg}) #000;
+    background-size: cover;
+    background-position: center;
+  }
+`;
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <GlobalStyles />
+    <App />
+  </React.StrictMode>,
+);
